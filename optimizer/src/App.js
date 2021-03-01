fe55161db98d8c3a1de699a092fb70453a13c7c1
ansnoussi <snoussi.anis@insat.u-carthage.tn>
2021-03-02 00:24:50 +0100

    all layout in place

--- optimizer/src/App.js
+++ optimizer/src/App.js
@@ -1,15 +1,231 @@
+import React from "react"
+
 import './App.css';
 import 'antd/dist/antd.css';
 
 
-import { Layout, Menu, Typography } from 'antd';
-import { UploadOutlined, UserOutlined, VideoCameraOutlined } from '@ant-design/icons';
+import { Layout, Menu, Typography, Table, Tag, Space, Drawer, Form, Button, Col, Row, Input, Select, DatePicker } from 'antd';
+import { UploadOutlined, UserOutlined, VideoCameraOutlined, PlusOutlined } from '@ant-design/icons';
 
 const { Header, Content, Footer, Sider } = Layout;
 const { Title } = Typography;
+const { Option } = Select;
 
-function App() {
+const columns = [
+  {
+    title: 'Name',
+    dataIndex: 'name',
+    key: 'name',
+    render: text => <a>{text}</a>,
+  },
+  {
+    title: 'Age',
+    dataIndex: 'age',
+    key: 'age',
+  },
+  {
+    title: 'Address',
+    dataIndex: 'address',
+    key: 'address',
+  },
+  {
+    title: 'Tags',
+    key: 'tags',
+    dataIndex: 'tags',
+    render: tags => (
+      <>
+        {tags.map(tag => {
+          let color = tag.length > 5 ? 'geekblue' : 'green';
+          if (tag === 'loser') {
+            color = 'volcano';
+          }
+          return (
+            <Tag color={color} key={tag}>
+              {tag.toUpperCase()}
+            </Tag>
+          );
+        })}
+      </>
+    ),
+  },
+  {
+    title: 'Action',
+    key: 'action',
+    render: (text, record) => (
+      <Space size="middle">
+        <a>Invite {record.name}</a>
+        <a>Delete</a>
+      </Space>
+    ),
+  },
+];
+
+const data = [
+  {
+    key: '1',
+    name: 'John Brown',
+    age: 32,
+    address: 'New York No. 1 Lake Park',
+    tags: ['nice', 'developer'],
+  },
+  {
+    key: '2',
+    name: 'Jim Green',
+    age: 42,
+    address: 'London No. 1 Lake Park',
+    tags: ['loser'],
+  },
+  {
+    key: '3',
+    name: 'Joe Black',
+    age: 32,
+    address: 'Sidney No. 1 Lake Park',
+    tags: ['cool', 'teacher'],
+  },
+];
+
+
+class App extends React.Component {
+
+  state = { visible: false };
+
+  showDrawer = () => {
+    this.setState({
+      visible: true,
+    });
+  };
+
+  onClose = () => {
+    this.setState({
+      visible: false,
+    });
+  };
+
+  renderDrawer() {
+    return(
+      <Drawer
+      title="Create a new account"
+      width={720}
+      onClose={this.onClose}
+      visible={this.state.visible}
+      bodyStyle={{ paddingBottom: 80 }}
+      footer={
+        <div
+          style={{
+            textAlign: 'right',
+          }}
+        >
+          <Button onClick={this.onClose} style={{ marginRight: 8 }}>
+            Cancel
+          </Button>
+          <Button onClick={this.onClose} type="primary">
+            Submit
+          </Button>
+        </div>
+      }
+    >
+      <Form layout="vertical" hideRequiredMark>
+        <Row gutter={16}>
+          <Col span={12}>
+            <Form.Item
+              name="name"
+              label="Name"
+              rules={[{ required: true, message: 'Please enter user name' }]}
+            >
+              <Input placeholder="Please enter user name" />
+            </Form.Item>
+          </Col>
+          <Col span={12}>
+            <Form.Item
+              name="url"
+              label="Url"
+              rules={[{ required: true, message: 'Please enter url' }]}
+            >
+              <Input
+                style={{ width: '100%' }}
+                addonBefore="http://"
+                addonAfter=".com"
+                placeholder="Please enter url"
+              />
+            </Form.Item>
+          </Col>
+        </Row>
+        <Row gutter={16}>
+          <Col span={12}>
+            <Form.Item
+              name="owner"
+              label="Owner"
+              rules={[{ required: true, message: 'Please select an owner' }]}
+            >
+              <Select placeholder="Please select an owner">
+                <Option value="xiao">Xiaoxiao Fu</Option>
+                <Option value="mao">Maomao Zhou</Option>
+              </Select>
+            </Form.Item>
+          </Col>
+          <Col span={12}>
+            <Form.Item
+              name="type"
+              label="Type"
+              rules={[{ required: true, message: 'Please choose the type' }]}
+            >
+              <Select placeholder="Please choose the type">
+                <Option value="private">Private</Option>
+                <Option value="public">Public</Option>
+              </Select>
+            </Form.Item>
+          </Col>
+        </Row>
+        <Row gutter={16}>
+          <Col span={12}>
+            <Form.Item
+              name="approver"
+              label="Approver"
+              rules={[{ required: true, message: 'Please choose the approver' }]}
+            >
+              <Select placeholder="Please choose the approver">
+                <Option value="jack">Jack Ma</Option>
+                <Option value="tom">Tom Liu</Option>
+              </Select>
+            </Form.Item>
+          </Col>
+          <Col span={12}>
+            <Form.Item
+              name="dateTime"
+              label="DateTime"
+              rules={[{ required: true, message: 'Please choose the dateTime' }]}
+            >
+              <DatePicker.RangePicker
+                style={{ width: '100%' }}
+                getPopupContainer={trigger => trigger.parentElement}
+              />
+            </Form.Item>
+          </Col>
+        </Row>
+        <Row gutter={16}>
+          <Col span={24}>
+            <Form.Item
+              name="description"
+              label="Description"
+              rules={[
+                {
+                  required: true,
+                  message: 'please enter url description',
+                },
+              ]}
+            >
+              <Input.TextArea rows={4} placeholder="please enter url description" />
+            </Form.Item>
+          </Col>
+        </Row>
+      </Form>
+    </Drawer>
+    )
+  }
+
+render() {
   return (
+    <>
     <Layout style={{minHeight:"100vh"}} > 
     <Sider
       breakpoint="lg"
@@ -44,14 +260,27 @@ function App() {
     <Layout>
       <Header className="site-layout-sub-header-background" style={{ padding: 0 }} />
       <Content style={{ margin: '24px 16px 0' }}>
+
+
         <div className="site-layout-background" style={{ padding: 24, minHeight: 360 }}>
-          content
+
+
+          <Button type="primary" onClick={this.showDrawer}>
+            <PlusOutlined /> New account
+          </Button>
+
+          <Table columns={columns} dataSource={data} />
         </div>
+
+
       </Content>
       <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
     </Layout>
   </Layout>
+  {this.renderDrawer()}
+  </>
   );
+}
 }
 
 export default App;
