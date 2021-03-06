a10122fd9824d5d97808a9660f03ac28f627f8b6
ansnoussi <snoussi.anis@insat.u-carthage.tn>
2021-03-06 21:47:32 +0100

    fixed it a little

--- sim.py
+++ sim.py
@@ -21,7 +21,7 @@ print( "total possible permutations: ",  len(all_permutations))
 
 
 # received activity
-ac0 = 3825
+ac0 = 3341
 # received volume
 v0 = 8.5
 # half life of the radioactive substance
@@ -35,11 +35,22 @@ def final_activity(patients_list):
     print(total)
     # I will test it first with intial patients list
     for i in range(len(patients)):
+        # we remove the injected quqntity
         total = total - patients[i][0]
-        l = math.log(2/half_life)
+        # we calculate the radioactive decay during the test time
+        lmd = math.log(2) / half_life
         t = patients[i][1]
-        lost_ac = math.exp(-(l*t))
-        total = total - math.floor(lost_ac)
+        lost_ac = math.exp( - ( lmd * t ) )
+        total = total - total * math.floor(lost_ac)
         print(total)
 
 final_activity([])
+
+
+
+# =D27*EXP(
+#     -LN(2) *  (((HOUR(E20)*60)+MINUTE(E20))-((HOUR(D20)*60)+MINUTE(D20)))  /  ($B$5)
+#     )
+
+
+# = a0 e( -ln(2) )
