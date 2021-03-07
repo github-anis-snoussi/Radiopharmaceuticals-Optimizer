3cb5720d627afa175b6e68cad2ae2382f0c454f0
ansnoussi <snoussi.anis@insat.u-carthage.tn>
2021-03-07 22:24:22 +0100

    some notes

--- sim.py
+++ sim.py
@@ -20,6 +20,11 @@ all_permutations = list(itertools.permutations(patients, len(patients)))
 print( "total possible permutations: ",  len(all_permutations))
 
 
+
+# λ = ln(2) / t (half)
+# A = A0 * e ^ (-λt)
+
+
 # received activity
 ac0 = 3341
 # received volume
@@ -29,6 +34,8 @@ half_life = 109.8
 # we received this at t = 0s
 t0 = 0
 
+decay_const = math.log(2) / half_life
+
 # function to calculate the remaining activity at the end of the test
 def final_activity(patients_list):
     total = ac0
@@ -36,21 +43,21 @@ def final_activity(patients_list):
     # I will test it first with intial patients list
     for i in range(len(patients)):
         # we remove the injected quqntity
-        total = total - patients[i][0]
+
+        # total = total - patients[i][0]
+
         # we calculate the radioactive decay during the test time
-        lmd = math.log(2) / half_life
-        t = patients[i][1]
-        lost_ac = math.exp( - ( lmd * t ) )
-        total = total - total * math.floor(lost_ac)
-        print(total)
+
+        elapsed_time = 0
+
+        for j in range(0, i):
+            elapsed_time = elapsed_time + patients[j][1] * 60
+        
+
+        # A = A0 * e ^ (-λt)
+        lost_ac = int( ac0 * math.exp( - ( decay_const * elapsed_time ) ) )
+
+        print(total - lost_ac)
 
 final_activity([])
 
-
-
-# =D27*EXP(
-#     -LN(2) *  (((HOUR(E20)*60)+MINUTE(E20))-((HOUR(D20)*60)+MINUTE(D20)))  /  ($B$5)
-#     )
-
-
-# = a0 e( -ln(2) )
