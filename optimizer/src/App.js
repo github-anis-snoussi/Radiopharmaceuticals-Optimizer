b3db0efdad5bf422c5b0eba43980749505e5c4b1
ansnoussi <snoussi.anis@insat.u-carthage.tn>
2021-03-18 02:17:29 +0100

    super tired

--- optimizer/src/App.js
+++ optimizer/src/App.js
@@ -18,12 +18,11 @@ import {
   Col, 
   Row, 
   Input, 
-  Select, 
-  DatePicker,
+  Select,
   Modal,
   InputNumber,
   TimePicker,
-  Divider
+  Divider,
 } from 'antd';
 
 import { 
@@ -147,9 +146,10 @@ class App extends React.Component {
         </div>
       }
     >
-      <Form layout="vertical" hideRequiredMark>
+      <Form layout="vertical" >
+
         <Row gutter={16}>
-          <Col span={12}>
+          <Col span={24}>
             <Form.Item
               name="name"
               label="Name"
@@ -158,89 +158,32 @@ class App extends React.Component {
               <Input placeholder="Please enter user name" />
             </Form.Item>
           </Col>
-          <Col span={12}>
-            <Form.Item
-              name="url"
-              label="Url"
-              rules={[{ required: true, message: 'Please enter url' }]}
-            >
-              <Input
-                style={{ width: '100%' }}
-                addonBefore="http://"
-                addonAfter=".com"
-                placeholder="Please enter url"
-              />
-            </Form.Item>
-          </Col>
         </Row>
+
         <Row gutter={16}>
+          
           <Col span={12}>
             <Form.Item
-              name="owner"
-              label="Owner"
-              rules={[{ required: true, message: 'Please select an owner' }]}
+              name="weight"
+              label="Weight (Kg)"
+              rules={[{ required: true, message: 'Please enter the weight' }]}
             >
-              <Select placeholder="Please select an owner">
-                <Option value="xiao">Xiaoxiao Fu</Option>
-                <Option value="mao">Maomao Zhou</Option>
-              </Select>
+              <InputNumber style={{width : "100%"}} min={0} />
             </Form.Item>
           </Col>
+
           <Col span={12}>
             <Form.Item
-              name="type"
-              label="Type"
-              rules={[{ required: true, message: 'Please choose the type' }]}
+              name="duration"
+              label="Test Duration (min)"
+              rules={[{ required: true, message: 'Please enter the duration' }]}
             >
-              <Select placeholder="Please choose the type">
-                <Option value="private">Private</Option>
-                <Option value="public">Public</Option>
-              </Select>
+              <InputNumber style={{width : "100%"}} min={0} />
             </Form.Item>
           </Col>
+
         </Row>
-        <Row gutter={16}>
-          <Col span={12}>
-            <Form.Item
-              name="approver"
-              label="Approver"
-              rules={[{ required: true, message: 'Please choose the approver' }]}
-            >
-              <Select placeholder="Please choose the approver">
-                <Option value="jack">Jack Ma</Option>
-                <Option value="tom">Tom Liu</Option>
-              </Select>
-            </Form.Item>
-          </Col>
-          <Col span={12}>
-            <Form.Item
-              name="dateTime"
-              label="DateTime"
-              rules={[{ required: true, message: 'Please choose the dateTime' }]}
-            >
-              <DatePicker.RangePicker
-                style={{ width: '100%' }}
-                getPopupContainer={trigger => trigger.parentElement}
-              />
-            </Form.Item>
-          </Col>
-        </Row>
-        <Row gutter={16}>
-          <Col span={24}>
-            <Form.Item
-              name="description"
-              label="Description"
-              rules={[
-                {
-                  required: true,
-                  message: 'please enter url description',
-                },
-              ]}
-            >
-              <Input.TextArea rows={4} placeholder="please enter url description" />
-            </Form.Item>
-          </Col>
-        </Row>
+
       </Form>
     </Drawer>
     )
