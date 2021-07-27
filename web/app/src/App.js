07a73e1c0cd84f8aa89b973e8db0a6f9e0e875c2
Anis Snoussi <snoussi.anis@insat.u-carthage.tn>
2021-07-27 17:58:18 +0200

    logo in place

--- web/app/src/App.js
+++ web/app/src/App.js
@@ -478,7 +478,59 @@ class App extends React.Component {
               console.log(collapsed, type);
             }}
           >
-            <Title className="logo">Optimizer</Title>
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "row",
+                justifyContent: "center",
+                alignItems: "center",
+                margin: 10,
+              }}
+            >
+              <img
+                src={"logo-filled-white.png"}
+                style={{ height: 60, aspectRatio: 1 }}
+              />
+
+              <div
+                style={{
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  flexDirection: "column",
+                  padding: 0,
+                  margin: 0,
+                }}
+              >
+                <span
+                  style={{
+                    fontSize: 20,
+                    fontWeight: "bold",
+                    color: "white",
+                    textAlign: "center",
+                    padding: 0,
+                    margin: 0,
+                    height: 20,
+                  }}
+                >
+                  RP
+                </span>
+                <span
+                  style={{
+                    fontSize: 20,
+                    fontWeight: "bold",
+                    color: "white",
+                    textAlign: "center",
+                    padding: 0,
+                    margin: 0,
+                    height: 20,
+                    marginBottom: 15,
+                  }}
+                >
+                  Optimizer
+                </span>
+              </div>
+            </div>
             <Menu
               theme="dark"
               mode="inline"
