5a6efc2035cde39d1273b9e716e0479a9bf31089
ansnoussi <snoussi.anis@insat.u-carthage.tn>
2021-12-25 00:00:22 +0100

    table colums in new file

--- app/src/Components/PatientsTable.js
+++ app/src/Components/PatientsTable.js
@@ -1,45 +1,15 @@
 import React from "react";
-import {
-  Table,
-  Space,
-  Button,
-  Popconfirm,
-  message,
-  Spin,
-  Typography,
-  DatePicker,
-} from "antd";
-import {
-  MenuOutlined,
-  ClockCircleOutlined,
-  CheckCircleOutlined,
-  ExclamationCircleOutlined,
-  DeleteOutlined,
-  EditOutlined,
-  ExperimentOutlined
-} from "@ant-design/icons";
-import {
-  sortableContainer,
-  sortableElement,
-  sortableHandle,
-} from "react-sortable-hoc";
+import { Table } from "antd";
+import { sortableContainer, sortableElement } from "react-sortable-hoc";
 import arrayMove from "array-move";
-import { confirmInjection } from "../utils/utils"
+import TableColums from "./TableColums"
 
-
-
-const { Text } = Typography;
 const SortableItem = sortableElement((props) => <tr {...props} />);
 const SortableContainer = sortableContainer((props) => <tbody {...props} />);
-const DragHandle = sortableHandle(() => (
-  <MenuOutlined style={{ cursor: "pointer", color: "#999" }} />
-));
-
 
 class PatientsTable extends React.Component {
   constructor(props) {
     super(props);
-    this.tableColums = this.tableColums.bind(this);
   }
 
   onSortEnd = ({ oldIndex, newIndex }) => {
@@ -87,189 +57,14 @@ class PatientsTable extends React.Component {
     updateData([...dataSource]);
   };
 
-  tableColums = () => {
-    const columns = [
-      {
-        title: "",
-        dataIndex: "sort",
-        width: 30,
-        className: "drag-visible",
-        render: (a, b) => {
-          if (b.status === "waiting") {
-            return <DragHandle />;
-          }
-        },
-      },
-      {
-        title: "Name",
-        dataIndex: "name",
-        key: "name",
-        render: (text) =><Text strong >{text}</Text>,
-      },
-      {
-        title: () => {return <div style={{textAlign : 'center'}} > <ExperimentOutlined/> Injection time</div>},
-        dataIndex: "expected_injection_time",
-        key: "expected_injection_time",
-        render(text) {
-          return {
-            props: {
-              style: { background: "#fffbe6" }
-            },
-            children: <Text>{text || '?'}</Text>
-          };
-        }
-      },
-      {
-        title: () => {return <div style={{textAlign : 'center'}} > <ExperimentOutlined/> Injection volume (ml) </div>},
-        dataIndex: "expected_injection_volume",
-        key: "expected_injection_volume",
-        render(text) {
-          return {
-            props: {
-              style: { background: "#fffbe6" }
-            },
-            children: <Text>{text || '?'}</Text>
-          };
-        }
-      },
-      {
-        title: "Dose (MBq)",
-        dataIndex: "dose",
-        key: "dose",
-      },
-      {
-        title: "Test Duration (min)",
-        dataIndex: "duration",
-        key: "duration",
-        render: (text) => <Text>{text}</Text>,
-      },
-      {
-        title: "Actions",
-        key: "action",
-        render: (text, record) => (
-          <Space>
-            <Button
-              size="small"
-              type="primary"
-              ghost
-              disabled={record.status !== "waiting"}
-              onClick={() => {
-                this.props.modifyPatient(record);
-              }}
-            >
-              <EditOutlined />
-              Modify
-            </Button>
-
-            {record.status === "waiting" ? (
-              <Popconfirm
-                title={"Proceed to delete ?"}
-                icon={<ExclamationCircleOutlined style={{ color: "red" }} />}
-                onConfirm={() => {
-                  this.props.deletePatient(record);
-                }}
-                okText="Delete Patient"
-                okButtonProps={{
-                  danger: true,
-                }}
-                cancelText="Cancel"
-              >
-                <Button danger size="small">
-                  <DeleteOutlined />
-                  Delete
-                </Button>
-              </Popconfirm>
-            ) : (
-              <Button danger size="small" disabled>
-                <DeleteOutlined />
-                Delete
-              </Button>
-            )}
-
-            {record.status === "waiting" ? (
-              <Popconfirm
-                title={
-                  <>
-                    <div>Select Injection Time for {record.name}</div>
-                    <DatePicker
-                      showTime
-                      size="small"
-                      style={{
-                        width: "100%",
-                        marginRight: 10,
-                        marginTop: 10,
-                        marginBottom: 10,
-                      }}
-                      onSelect={(mesure_time) => {
-                        this.updateRecordMeasureTime(record, mesure_time);
-                      }}
-                    />
-                  </>
-                }
-                onConfirm={() => {
-                  if (!record.realInjectionTime) {
-                    message.warning("Please select Injection time first.");
-                  } else {
-                    confirmInjection(
-                      record,
-                      this.props.dataSource,
-                      this.props.updateData
-                    );
-                  }
-                }}
-                okText="Inject"
-                cancelText="Cancel"
-              >
-                <Button size="small"> 💉 Inject {record.name}</Button>
-              </Popconfirm>
-            ) : (
-              <Button size="small" disabled>
-                💉 Inject {record.name}
-              </Button>
-            )}
-          </Space>
-        ),
-      },
-      {
-        title: "Status",
-        key: "status",
-        dataIndex: "status",
-        render: (status) => {
-          switch (status) {
-            case "done":
-              return (
-                <CheckCircleOutlined
-                  style={{ color: "green", fontSize: "23px" }}
-                />
-              );
-            case "test":
-              return <Spin />;
-            case "waiting":
-              return (
-                <ClockCircleOutlined
-                  style={{ color: "orange", fontSize: "23px" }}
-                />
-              );
-            default:
-              return (
-                <ExclamationCircleOutlined
-                  style={{ color: "red", fontSize: "23px" }}
-                />
-              );
-          }
-        },
-      },
-    ];
-
-    return columns;
-  };
 
   render() {
+    const { modifyPatient, deletePatient, updateRecordMeasureTime, dataSource, updateData } = this.props;
     return (
       <Table
         pagination={false}
-        dataSource={this.props.dataSource}
-        columns={this.tableColums()}
+        dataSource={dataSource}
+        columns={TableColums(modifyPatient, deletePatient, updateRecordMeasureTime, dataSource, updateData)}
         rowKey="index"
         components={{
           body: {
