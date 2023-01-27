d86a47c3d8e3d18aa17fa9f787e97d84dfa12d8a
Anis Snoussi <anis@getleeway.com>
2023-01-27 22:26:12 +0100

    any everywhere

--- app/src/utils/utils.ts
+++ app/src/utils/utils.ts
@@ -2,12 +2,12 @@ import { message } from "antd";
 import { clean } from "./sortPatientList";
 import { sendAmplitudeData, amplitudeLogsTypes } from "./amplitude";
 
-export function confirmInjection(record, patientsList, updateData) {
+export function confirmInjection(record: any, patientsList: any, updateData: any) {
   if (record.realInjectionTime === null) {
     message.warning("Operation aborted");
   } else {
     // we change the status to injected
-    patientsList.forEach(function (part, index, theArray) {
+    patientsList.forEach(function (part: any, index: any, theArray: any) {
       if (theArray[index].index === record.index) {
         theArray[index].isInjected = true;
       }
