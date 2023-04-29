e7803985dfed46c15dd0814ce93446c4233294c8
Anis Snoussi <anis@getleeway.com>
2023-04-30 00:02:59 +0100

    more unit tests

--- app/src/core/maths.ts
+++ app/src/core/maths.ts
@@ -6,15 +6,31 @@ import { RpSettingsType } from "../context/RpSettingsContext"
 * @param {number} date1 - The start time in ms
 * @param {number} date1 - The end time in ms
 */
-export const diffMsTimeMinutes = (date1: number, date2: number) => {
+export const diffMsTimeMinutes = (date1: number, date2: number): number => {
     const diffMs = Math.abs(date1 - date2);
     const diffMinutes = Math.round(diffMs / 60000);
     return diffMinutes;
 };
 
-export const decay = (a0: number, halfLife: number, t: number) => {
+/**
+* Calculates the radioactive decay A = A0 * e^(-ln(2/halfLife))t
+* @param {number} a0 - The starting activity (MBq)
+* @param {number} halfLife - the halflife value of the radioactive substance (min)
+* @param {number} t - elapsed time (min)
+*/
+export const decay = (a0: number, halfLife: number, t: number): number => {
     const a = a0 * Math.exp((-Math.log(2) * t) / halfLife);
     return a;
+};
+
+/**
+* Calculates the usable radioactivity given the total recieved radioactive substance, and the volume that cannot be extracted from the container
+* @param {number} totalRpActivity - The total radioactivity of the recieved substance (MBq)
+* @param {number} totalRpVol - The total volume of the recieved substance (ml)
+* @param {number} unextractableRpVol - the volume of the substance that cannot be extracted from the container (ml)
+*/
+export const usableActivity = (totalRpActivity: number, totalRpVol: number, unextractableRpVol: number): number => {
+    return (totalRpActivity * (totalRpVol - unextractableRpVol)) / totalRpVol;
 };
 
 export const activityAtFirstInj = (patientInjTimeList: number[], rpSettings: RpSettingsType) => {
@@ -26,10 +42,6 @@ export const activityAtFirstInj = (patientInjTimeList: number[], rpSettings: RpS
     const ra =
         (ta * (rpSettings.rpVol - rpSettings.wastedVol)) / rpSettings.rpVol;
     return ra;
-};
-
-export const usableActivity = (totalRpActivity: number, totalRpVol: number, unextractableRpVol: number) => {
-    return (totalRpActivity * (totalRpVol - unextractableRpVol)) / totalRpVol;
 };
 
 export const generatePatientInjTimeList = (
